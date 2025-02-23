f90101acd94c6ea5b5937d3230522ff587106a86
Awoke Gebrie <148381739+Awe360@users.noreply.github.com>
2025-02-23 15:48:36 +0300

    Update generateTokenAndSetCookie.js

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -5,20 +5,14 @@ export const generateTokenAndSetCookie = (res, userId) => {
 		expiresIn: "7d",
 	});
 
-	// res.cookie("token", token, {
-	// 	httpOnly: true,
-	// 	sameSite: "None", 
-	// 	secure: true,
-	// 	maxAge: 60 * 60 * 24 * 7,
-	// 	path: "/", 
-	// });
 	res.cookie("token", token, {
-  httpOnly: true,  // Prevent JavaScript access
-  secure: true,    // Must be true for HTTPS (Vercel & Render are HTTPS)
-  sameSite: "strict", // Required for cross-origin requests
-  path: "/",
-  maxAge: 24 * 60 * 60 * 1000,  // 1-day expiration
-});
+		httpOnly: true,
+		sameSite: "None", 
+		secure: true,
+		maxAge: 60 * 60 * 24 * 7,
+		path: "/", 
+	});
+
 
 	
 	
