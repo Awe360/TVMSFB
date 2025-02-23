e709cb19fb990839185e6323d76324c3907942d2
Awoke Gebrie <148381739+Awe360@users.noreply.github.com>
2025-02-23 14:25:34 +0300

    Update generateTokenAndSetCookie.js

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -5,13 +5,19 @@ export const generateTokenAndSetCookie = (res, userId) => {
 		expiresIn: "7d",
 	});
 
+	// res.cookie("token", token, {
+	// 	httpOnly: true,
+	// 	sameSite: "None", 
+	// 	secure: true,
+	// 	maxAge: 60 * 60 * 24 * 7,
+	// 	path: "/", 
+	// });
 	res.cookie("token", token, {
-		httpOnly: true,
-		sameSite: "None", 
-		secure: true,
-		maxAge: 60 * 60 * 24 * 7,
-		path: "/", 
-	});
+    httpOnly: true,
+    secure: false, // Set to false for localhost (HTTP)
+    sameSite: "none", // Allow cross-origin cookies
+    maxAge: 60 * 60 * 24 * 7, // 1 week
+});
 	
 	return token;
 };
