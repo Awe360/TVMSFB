a1990ff92db32675e3970323bf43e91a6d8cdd7e
Awoke Gebrie <148381739+Awe360@users.noreply.github.com>
2025-02-23 21:47:15 +0300

    Update generateTokenAndSetCookie.js

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
 	res.cookie("token", token, {
 		httpOnly:true,
-		sameSite: "strict", 
+		sameSite: "none", 
 		secure: true,
 		expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
 		path: "/", 
