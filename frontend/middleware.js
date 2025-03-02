386cd3be52480d04a3e29001392f7bcc85310877
Awoke Gebrie <148381739+Awe360@users.noreply.github.com>
2025-03-03 01:00:24 +0300

    Update middleware.js

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -1,39 +1,39 @@
-import { NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+// import { NextResponse } from "next/server";
+// import { jwtVerify } from "jose";
 
-export async function middleware(req) {
-    const token = req.cookies.get("token")?.value || req.headers.get("cookie")?.split("token=")[1]?.split(";")[0];
-    const navigatingRoute = req.nextUrl.pathname;
+// export async function middleware(req) {
+//     const token = req.cookies.get("token")?.value || req.headers.get("cookie")?.split("token=")[1]?.split(";")[0];
+//     const navigatingRoute = req.nextUrl.pathname;
 
-    console.log("Protected route:", navigatingRoute);
+//     console.log("Protected route:", navigatingRoute);
 
-    if (token) {
-        try {
-            const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-            const decoded = await jwtVerify(token, secret);
+//     if (token) {
+//         try {
+//             const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+//             const decoded = await jwtVerify(token, secret);
             
 
-            // If logged-in user tries to access "/auth/login", redirect to admin dashboard
-            if (decoded && navigatingRoute.startsWith("/auth")) {
-                return NextResponse.redirect(new URL("/admin/home", req.url));
-            }
-        } catch (error) {
-            console.log("Error verifying token:", error);
-            return NextResponse.redirect(new URL("/auth/login", req.url));
-        }
-    } else {
-        // Allow unauthenticated access to "/auth/login"
-        if (navigatingRoute.startsWith("/auth")) {
-            return NextResponse.next();
-        }
+//             // If logged-in user tries to access "/auth/login", redirect to admin dashboard
+//             if (decoded && navigatingRoute.startsWith("/auth")) {
+//                 return NextResponse.redirect(new URL("/admin/home", req.url));
+//             }
+//         } catch (error) {
+//             console.log("Error verifying token:", error);
+//             return NextResponse.redirect(new URL("/auth/login", req.url));
+//         }
+//     } else {
+//         // Allow unauthenticated access to "/auth/login"
+//         if (navigatingRoute.startsWith("/auth")) {
+//             return NextResponse.next();
+//         }
 
-        console.log("No token found, redirecting...");
-        return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
+//         console.log("No token found, redirecting...");
+//         return NextResponse.redirect(new URL("/auth/login", req.url));
+//     }
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ["/admin/:path*", "/profile/:path*","/auth/:path*"]
-};
+// export const config = {
+//     matcher: ["/admin/:path*", "/profile/:path*","/auth/:path*"]
+// };
